ba678e4c05ea06b0920791f56160dcbcbdc63cf5
Tobias Halomoan <agyasta1808@gmail.com>
2025-03-25 07:43:01 +0700

    fix : updated create_app for deployment

--- app.py
+++ app.py
@@ -1,49 +1,5 @@
-from flask import Flask, jsonify
-from app.config.config import Config
-from app.config.env import init_env
-from app.extensions import db, jwt
-from app.controllers.auth_controller import auth_bp
-from app.controllers.protected_controller import protected_bp
-from app.controllers.partner_controller import partner_bp
-from app.controllers.outlet_controller import outlet_bp
-from app.controllers.product_controller import product_bp
-from app.controllers.expense_category_controller import expense_category_bp
-from app.controllers.income_category_controller import income_category_bp
-from app.controllers.reports_controller import reports_bp
-from app.controllers.manual_entry_controller import manual_entries_bp
-from flask_cors import CORS
-
-def create_app():
-    # Initialize environment variables
-    init_env()
-    app = Flask(__name__)
-    app.config.from_object(Config)
-
-    # Initialize extensions AFTER app creation
-    db.init_app(app)
-    jwt.init_app(app)
-
-     # Enable CORS for all domains and ports
-    CORS(app)
-
-    # Register Blueprints (Routes)
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(protected_bp)
-    app.register_blueprint(partner_bp)
-    app.register_blueprint(outlet_bp)
-    app.register_blueprint(product_bp)
-    app.register_blueprint(expense_category_bp)
-    app.register_blueprint(income_category_bp)
-    app.register_blueprint(reports_bp)
-    app.register_blueprint(manual_entries_bp)
-
-
-    @app.route('/')
-    def welcome():
-        return jsonify({'message': 'Welcome to the MP78 API'})
-    
-
-    return app
+from app import create_app
+from app.extensions import db
 
 if __name__ == '__main__':
     app = create_app()
