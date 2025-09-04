ab267de764939ec10192fe10b3ebd9c67e199b13
Tobias Halomoan <agyasta1808@gmail.com>
2025-09-04 13:39:36 +0700

    fix : used int for cash income and expense

--- app/controllers/export_controller.py
+++ app/controllers/export_controller.py
@@ -214,13 +214,15 @@ def export_reports():
             date = report.tanggal.date()
             if date not in daily_totals:
                 daily_totals[date] = init_daily_total()
-            daily_totals[date]['Cash_Income'] += float(report.total or 0)
+            daily_totals[date]['Cash_Income'] = int(
+            daily_totals[date]['Cash_Income'] + float(report.total or 0))
 
         for report in cash_expense_reports:
             date = report.tanggal.date()
             if date not in daily_totals:
                 daily_totals[date] = init_daily_total()
-            daily_totals[date]['Cash_Expense'] += float(report.total or 0)
+            daily_totals[date]['Cash_Expense'] = int(
+            daily_totals[date]['Cash_Expense'] + float(report.total or 0))
 
         # Add mutation matching logic for each platform
         platforms = ['gojek', 'grab', 'shopee','shopeepay']
